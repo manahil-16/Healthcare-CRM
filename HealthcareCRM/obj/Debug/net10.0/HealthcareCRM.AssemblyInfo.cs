27422f6f99a59212c3c5d54fb54c6608449be1f6
01-131232-044 <01-131232-044@student.bahria.edu.pk>
2026-07-22 14:41:49 +0500

    feat(rbac): add admin panel, role-based access control and navbar updates

--- HealthcareCRM/obj/Debug/net10.0/HealthcareCRM.AssemblyInfo.cs
+++ HealthcareCRM/obj/Debug/net10.0/HealthcareCRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b2931493512943a0b35f228c9ecc5f1e2fcd33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e6e20a62bf37a1789bc28925c411f0c1b428b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
